d8af4a1a44868458bff67099892dc0ce619c0be0
israelcampos011 <163329731+israelcampos011@users.noreply.github.com>
2024-05-29 23:00:05 -0600

    Add files via upload

---
+++ Leds1/Leds1.ino
@@ -0,0 +1,13 @@
+void setup() {
+  pinMode(13,INPUT);
+  pinMode(12,INPUT);
+  pinMode(11,INPUT);
+  pinMode(10,INPUT);
+}
+
+void loop() {
+digitalWrite(13,1);
+digitalWrite(12,1);
+digitalWrite(11,1);
+digitalWrite(10,1);
+}
